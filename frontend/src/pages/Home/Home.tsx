868fdcb177302d62ca46d1478e922a72bdf19577
sandrorvr <sandrorvr@yahoo.com.br>
2023-10-13 11:59:42 -0300

    start

---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import './Home.css'
+
+function Home() {
+
+  return (
+    <div id='container'>
+      <section className='bannerSection'>
+        <span>Sandro Jr.</span>
+      </section>
+      <section className='postSection'></section>
+    </div>
+  )
+}
+
+export default Home;
